cd1fb8c069884d6630b96c13f06ad67ee6343d67
Jochen Keil <1392715+jchnkl@users.noreply.github.com>
2021-11-24 14:29:45 +0100

    Create example.php with some content

---
+++ example.php
@@ -0,0 +1,25 @@
+function say_hello() {
+  return "Hello!";
+}
+
+echo say_hello();
+
+// Simple Function + Parameter + Call
+
+function say_hello($friend) {
+  return "Hello " . $friend . "!";
+}
+
+echo say_hello('Tommy');
+
+// strtoupper - Makes all Chars BIGGER AND BIGGER!
+
+function makeItBIG($a_lot_of_names) {
+  foreach($a_lot_of_names as $the_simpsons) {
+    $BIG[] = strtoupper($the_simpsons);
+  }
+  return $BIG;
+}
+
+$a_lot_of_names = ['Homer', 'Marge', 'Bart', 'Maggy', 'Lisa'];
+var_dump(makeItBIG($a_lot_of_names));
